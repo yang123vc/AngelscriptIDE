67178d60dfb0514e8829e8e8c77d6e46cff17937
Sam <duo929@hotmail.com>
2017-06-02 12:31:57 +0200

    Refactor WinXPSupport.cmake to use macro instead of running on inclusion

--- cmake/WinXPSupport.cmake
+++ cmake/WinXPSupport.cmake
@@ -1,23 +1,16 @@
 #
-#	This file checks if thread-safe initialization needs to be disabled.
+#	This macro checks if thread-safe initialization needs to be disabled.
 #	If targeting Windows XP (v*_xp toolset) and using an MSVC version of Visual Studio 2015 or newer, it is disabled.
-#	This file must be included AFTER starting the project, else the required variables will not have been defined yet.
-#
+#	Must be called after starting a project, othewise the required variables will not have been defined yet.
+macro( check_winxp_support )
+	#Verify that we have the data we need.
+	if( NOT MSVC_VERSION OR NOT CMAKE_VS_PLATFORM_TOOLSET )
+		MESSAGE( FATAL_ERROR "Include WinXPSupport.cmake after starting a project" )
+	endif()
 
-#Avoid doing this more than once.
-if( WINXP_SUPPORT_CHECKED OR NOT MSVC )
-	return()
-endif()
-
-set( WINXP_SUPPORT_CHECKED 1 )
-
-#Verify that we have the data we need.
-if( NOT MSVC_VERSION OR NOT CMAKE_VS_PLATFORM_TOOLSET )
-	MESSAGE( FATAL_ERROR "Include WinXPSupport.cmake after starting a project" )
-endif()
-
-if( NOT "${MSVC_VERSION}" LESS 1900 AND CMAKE_VS_PLATFORM_TOOLSET MATCHES ".*_xp$" )
-	MESSAGE( STATUS "Disabling Thread-safe initialization for Windows XP support" )
-	#Disable thread-safe init so Windows XP users don't get crashes.
-	set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:threadSafeInit-" )
-endif()
+	if( NOT "${MSVC_VERSION}" LESS 1900 AND CMAKE_VS_PLATFORM_TOOLSET MATCHES ".*_xp$" )
+		MESSAGE( STATUS "Disabling Thread-safe initialization for Windows XP support" )
+		#Disable thread-safe init so Windows XP users don't get crashes.
+		set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:threadSafeInit-" )
+	endif()
+endmacro( check_winxp_support )
